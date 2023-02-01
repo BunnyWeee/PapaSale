9dde8b44524592b1f05693d2301c8571920afc3d
bunny <bunny_wei@hmg.com.tw>
2023-02-01 15:03:54 +0800

    default normal role for new user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,18 @@
 class User < ApplicationRecord
   rolify
+  after_create :assign_default_role
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
   has_many :posts, dependent: :destroy
 
+  private
+
+  def assign_default_role
+    self.add_role(:normal) if self.roles.blank?
+  end
 end
